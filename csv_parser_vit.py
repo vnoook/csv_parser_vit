470bdd7e4d022c77e0840e6a18330c1160bf3fc4
vnoook <noook@yandex.ru>
2022-12-28 20:36:57 +0700

    Initial commit

---
+++ csv_parser_vit.py
@@ -0,0 +1,21 @@
+import csv
+from pprint import pprint as pp
+
+file_csv_from_mis = 'out_data1.csv'
+staff_dict = {}
+
+with open(file_csv_from_mis, encoding='cp1251', newline='') as csvfile:
+    row_csv_content = csv.reader(csvfile, delimiter=';')
+
+    for row in row_csv_content:
+        if row_csv_content.line_num == 1:
+            staff_dict[row[9]] = ''
+        else:
+            if staff_dict.get(row[9], False):
+                print('est')
+                staff_dict[row[9]] = staff_dict[row[9]] + 1
+            else:
+                print('no')
+                staff_dict[row[9]] = 1
+
+pp(staff_dict)
